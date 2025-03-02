e4121bfa915c4b1c8c376972a818464d2aae6ed8
Mritunjay Patel <97620757+Mrpatel2319@users.noreply.github.com>
2025-03-02 14:31:03 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import React from "react";
-import './../styles/App.css';
-import PersonalizedGreeting from './PersonalizedGreeting';
 
 const App = () => {
+  const [input, setInput] = useState(''); 
+
+  const onInput = (e) => {
+    const { value } = e.target;
+    setInput(value);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
-        <PersonalizedGreeting />
+      {/* Do not remove the main div */}
+      <input value={input} onChange={onInput} />
+      {!!input.length && <p>Hello {input}!</p>}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
